f1b88aa55ebccbd20428dd87e8a01310268b7d74
cmelendez-coder <cmelendez@everscalegroup.com>
2026-08-12 12:18:41 -0600

    style: increase recruiter column to 300px

--- src/pages/Requirements.jsx
+++ src/pages/Requirements.jsx
@@ -1201,7 +1201,7 @@ function ReqBoardTable() {
 
   const COLS = [
     { label: 'Búsqueda',       width: '80px'  },
-    { label: 'Recruiter',      width: '200px' },
+    { label: 'Recruiter',      width: '300px' },
     { label: 'Prioridad',      width: '80px'  },
     { label: 'Cliente',        width: '130px' },
     { label: 'Position',       width: '210px' },
